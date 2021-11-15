92e7e35dbce89b88bf1b91b87bf107342097bd14
Omar Valdivia Samudio <omarvalsam@gmail.com>
2021-11-14 18:10:10 -0700

    function code fixed

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var special = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
-var Lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var Upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var number = "0123456789";
+var special = "@%+\/',!#$^? )(}{][~-_.";
+var Lower = "abcdefghijklmnopqrstuvwxyz";
+var Upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-var passwordammount=""
+var password ="";
+var passwordammount="";
 var conNumber;
 var conSpecial;
 var conLower;
 var conUpper;
 
-// Write password to the #password input
-function writePassword() { 
-
+function generatePassword() {
   var passwordcharacters = "";
   
   var passwordammount = window.prompt("How long would you like your password to be?");
@@ -61,17 +60,18 @@ function writePassword() {
         
         password += passwordcharacters [Math.floor(Math.random() * passwordcharacters.length)];
       }
+      console.log (password);
       return password;
-      }
+      };
  
-      console.log (password)
+  // Write password to the #password input
+function writePassword() { 
   
-  var passwordText = generatePassword();
+  var passwordtext = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
-    
+}    
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
